580cef3dd17dea42000c081bfef897b2e9f56bcd
yaroslav <yaroslav-tokar@yandex.ru>
2018-04-23 19:44:59 +0300

    Add liquibase.

--- src/main/java/com/sptest/server/config/AppConfig.java
+++ src/main/java/com/sptest/server/config/AppConfig.java
@@ -5,15 +5,23 @@ import com.sptest.server.repository.ILocalShopRepository;
 import com.sptest.server.repository.LocalShopRepository;
 import com.sptest.server.utils.DBConstants;
 import java.net.UnknownHostException;
+import javax.sql.DataSource;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -27,36 +35,95 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
         @ComponentScan("com.sptest.server.services"),
         @ComponentScan("com.sptest.server")
     })
+@PropertySource("classpath:local.properties")
 public class AppConfig {
 
-    @Bean
-    public LocalEntityManagerFactoryBean geEntityManagerFactoryBean() {
-        LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
-        factoryBean.setPersistenceUnitName("localPersistence");
-        return factoryBean;
-    }
+  @Value("${mysql.db.password}")
+  private String mysqlDatabasePassword;
 
-    @Bean
-    public JpaTransactionManager geJpaTransactionManager() {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(geEntityManagerFactoryBean().getObject());
-        return transactionManager;
-    }
+  @Value("${mysql.db.username}")
+  private String mysqlDatabaseUser;
 
-    @Bean
-    public ILocalShopRepository getShopRepository() {
-        return new LocalShopRepository();
-    }
+  @Value("${db.host}")
+  private String databaseHost;
 
-    @Bean
-    public MongoDbFactory mongoDbFactory() throws Exception {
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
-        UserCredentials userCredentials = new UserCredentials("", "");
-        return new SimpleMongoDbFactory(mongoClient, "mongo_test_db", userCredentials);
-    }
+  @Value("${mongo.db.port}")
+  private Integer mongoPort;
 
-    @Bean
-    public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongoDbFactory());
-    }
+  @Value("${mongo.db.database.name}")
+  private String mongoDbName;
+
+  @Value("${mongo.db.username}")
+  private String mongoUserName;
+
+  @Value("${mongo.db.password}")
+  private String mongoUserPassword;
+
+  @Value("${jpa.local.confg.profile}")
+  private String jpaLocalProfile;
+
+  @Value("${mysql.db.driver}")
+  private String mysqlDatabaseDriver;
+
+  @Value("${mysql.db.url}")
+  private String mysqlDatabaseUrl;
+
+  @Autowired
+  private Environment env;
+
+  @Bean
+  public LocalEntityManagerFactoryBean geEntityManagerFactoryBean() {
+    LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
+    factoryBean.setPersistenceUnitName(jpaLocalProfile);
+    return factoryBean;
+  }
+
+  @Bean
+  public JpaTransactionManager geJpaTransactionManager() {
+    JpaTransactionManager transactionManager = new JpaTransactionManager();
+    transactionManager.setEntityManagerFactory(geEntityManagerFactoryBean().getObject());
+    return transactionManager;
+  }
+
+  @Bean
+  public ILocalShopRepository getShopRepository() {
+    return new LocalShopRepository();
+  }
+
+  @Bean
+  public MongoDbFactory mongoDbFactory() throws Exception {
+    MongoClient mongoClient = new MongoClient(databaseHost, mongoPort);
+    UserCredentials userCredentials = new UserCredentials(mongoUserName, mongoUserPassword);
+    return new SimpleMongoDbFactory(mongoClient, mongoDbName, userCredentials);
+  }
+
+  @Bean
+  public MongoTemplate mongoTemplate() throws Exception {
+    return new MongoTemplate(mongoDbFactory());
+  }
+
+  @Bean
+  public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() {
+    return new PropertySourcesPlaceholderConfigurer();
+  }
+
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName(mysqlDatabaseDriver);
+    dataSource.setUrl(mysqlDatabaseUrl);
+    dataSource.setUsername(mysqlDatabaseUser);
+    dataSource.setPassword(mysqlDatabasePassword);
+    return dataSource;
+  }
+
+  /*@Bean
+  public SpringLiquibase liquibase() {
+    SpringLiquibase liquibase = new SpringLiquibase();
+    liquibase.setChangeLog("classpath:liquibase-changelog.xml");
+    liquibase.setDataSource(dataSource());
+    return liquibase;
+  }*/
+
+
 }
--- src/main/java/com/sptest/server/starter/Starter.java
+++ src/main/java/com/sptest/server/starter/Starter.java
@@ -8,7 +8,6 @@ import com.sptest.server.services.IMoneyReportService;
 import com.sptest.server.services.IRoleService;
 import com.sptest.server.services.MoneyReportService;
 import java.io.FileNotFoundException;
-import org.joda.time.LocalTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
@@ -16,48 +15,33 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Starter {
 
-    public static Logger logger = LoggerFactory.getLogger(Starter.class.getName());
+  public static Logger logger = LoggerFactory.getLogger(Starter.class.getName());
 
 
-    public static void main(String[] args) throws FileNotFoundException, BeansException {
-        logger.info("hello from sl4j");
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
-            AppConfig.class);
+  public static void main(String[] args) throws FileNotFoundException, BeansException {
+    logger.info("hello from sl4j");
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+        AppConfig.class);
 
-       /* IRoleService roleService = context.getBean(IRoleService.class);
-        RoleEntity role1 = new RoleEntity("student", 1200, 410113);
-        RoleEntity role2 = new RoleEntity("prepod", 8100, 4100115);
+    IMoneyReportService reportService = context.getBean(IMoneyReportService.class);
 
-        EmployeeEntity employeeEntity1 = new EmployeeEntity("Lola", 22, "Lola address");
-        EmployeeEntity employeeEntity2 = new EmployeeEntity("Masha", 23, "masha adress");
-        EmployeeEntity employeeEntity3 = new EmployeeEntity("Vika", 20, "vika address");
+    MoneyReport moneyReport1 = new MoneyReport(3412, "998", "245", "550.00f",
+        String.valueOf(System.currentTimeMillis()));
+    MoneyReport moneyReport2 = new MoneyReport(3411, "997", "246", "460.00f",
+        String.valueOf(System.currentTimeMillis()));
+    MoneyReport moneyReport3 = new MoneyReport(3410, "996", "227", "770.00f",
+        String.valueOf(System.currentTimeMillis()));
+    MoneyReport moneyReport5 = new MoneyReport(3409, "995", "248", "980.00f",
+        String.valueOf(System.currentTimeMillis()));
+    MoneyReport moneyReport4 = new MoneyReport(3408, "994", "249", "190.00f",
+        String.valueOf(System.currentTimeMillis()));
+    /*reportService.create(moneyReport1);
+    reportService.create(moneyReport2);
+    reportService.create(moneyReport3);
+    reportService.create(moneyReport4);
+    reportService.create(moneyReport5);*/
+    reportService.findAll().forEach(System.out::println);
 
-        role1.getEmployees().add(employeeEntity1);
-        role1.getEmployees().add(employeeEntity2);
-        role1.getEmployees().add(employeeEntity3);
-
-        role2.getEmployees().add(employeeEntity1);
-        role2.getEmployees().add(employeeEntity2);
-        role2.getEmployees().add(employeeEntity3);
-
-        roleService.save(role1);
-        roleService.save(role2);
-*/
-        IMoneyReportService reportService = context.getBean(IMoneyReportService.class);
-
-        MoneyReport moneyReport1 = new MoneyReport(1232,"12", "100", "124.45f", "23123123");
-        MoneyReport moneyReport2 = new MoneyReport(1221,"13", "200", "145325.45f",
-            String.valueOf(System.currentTimeMillis()));
-        MoneyReport moneyReport3 = new MoneyReport(1255,"14", "300", "19999.45f",
-            String.valueOf(System.currentTimeMillis()));
-        MoneyReport moneyReport4 = new MoneyReport(1299, "45","400", "145234.45f",
-            String.valueOf(System.currentTimeMillis()));
-        reportService.create(moneyReport1);
-        reportService.create(moneyReport2);
-        reportService.create(moneyReport3);
-        reportService.create(moneyReport4);
-        reportService.findAll().forEach(System.out::println);
-
-    }
+  }
 
 }
